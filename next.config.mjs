862a4c2d0103e5fefc32c4746113395028983d9c
SujalGour <sujalgaur70832@gmail.com>
2025-08-24 01:23:27 +0530

    cfg for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production'
+const repo = 'Portfolio'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',                 
+  basePath: isProd ? `/${repo}` : '',    
+  assetPrefix: isProd ? `/${repo}/` : '', 
+  images: { unoptimized: true },
   eslint: {
     ignoreDuringBuilds: true,
   },
